8d27132e16d62f4fa63669318b289dc9a6db93ec
Mukesh Prajapat <darwin.mukesh2022@gmail.com>
2023-07-05 13:42:33 +0530

    first lecture

---
+++ c++/let_1.cpp
@@ -0,0 +1,31 @@
+#include<iostream> // preprocessor
+using namespace std;
+int main()
+{
+     
+    int  number = 50;//integr --> -15 to +15  
+    float numberForFloat = 25.23;
+    int age;
+
+    cin>>age;
+    cout<<age<<endl;
+
+    
+//    cout<<"hello world"<<endl;
+//    cout<<"hello darwin "<<"my name is mukesh 25"<<endl;
+//    cout<<number<<endl;
+//    cout<<number+23<<endl;
+//    cout<<number-23<<endl;
+//    cout<<number*10<<endl; 
+//    cout<<number/10<<endl; 
+//    cout<<number%10<<endl; 
+//    cout<<"hello"<<endl<<"world";
+
+/*
+klgfkl
+k;fgddl;lgh
+*/
+
+
+    return 0;
+}
